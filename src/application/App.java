73da66c12e563a0421ddf87b4722cf07e63bc74f
Kevin Alexander <kev.alexander@outlook.com>
2022-06-24 14:47:29 -0300

    Seller entity class

--- src/application/App.java
+++ src/application/App.java
@@ -1,12 +1,22 @@
 package application;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(
+                21, "Bob", "bob@gmail.com",
+                new Date(sdf.parse("22/04/1985").getTime()),
+                3000.0, obj);
+
+        System.out.println(seller);
     }
 }
