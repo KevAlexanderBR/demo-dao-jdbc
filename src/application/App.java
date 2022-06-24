a85785076c2f989a4a1d59b1db09e6d6a4e1565b
Kevin Alexander <kev.alexander@outlook.com>
2022-06-24 16:53:23 -0300

    insert implementation

--- src/application/App.java
+++ src/application/App.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,7 +11,7 @@ import model.entities.Seller;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        SellerDao sellerDao =  DaoFactory.createSellerDao();
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ====");
         Seller seller = sellerDao.findById(3);
@@ -28,5 +29,10 @@ public class App {
         for (Seller objSeller : list) {
             System.out.println(objSeller);
         }
+
+        System.out.println("\n=== TEST 4: seller insert ====");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id = " + newSeller.getId());
     }
 }
